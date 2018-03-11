cc6365910a3bacc840fa28ffea4d10579324ea70
EchoHe <yxuhe@ucdavis.edu>
2018-03-11 14:00:34 -0700

    Update Neuron.m

--- Neuron.m
+++ Neuron.m
@@ -4,6 +4,6 @@ function [ OUT ] = Neuron( InputList, InputWeight )
 % Format for InputList [x1;x2;x3;etc]
 % Format for InputWeight [y1 y2 y3 etc];
 NET = InputWeight * InputList
-OUT = 1/(1+exp(NET));
+OUT = 1/(1+exp(-NET));
 end
 
