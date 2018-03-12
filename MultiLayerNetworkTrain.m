8380b1087e17b05356de00c561657140581676a8
Melanie Zhang <melanizhang@gmail.com>
2018-03-12 03:43:26 -0700

    changes to backpropagation, STILL NOT TESTED YET (:

--- MultiLayerNetworkTrain.m
+++ MultiLayerNetworkTrain.m
@@ -1,13 +1,25 @@
-function [ O ] = MultiLayerNetworkTrain(inputs, targets, weights, eta)
-% for now, i am assuming inputs and targets are cell arrays.
+function [ weights ] = MultiLayerNetworkTrain(inputs, targets, weights, eta)
+% PART 6 -- this probably doesn't work yet
+% for now, i am assuming inputs and targets are cell arrays...
 % if using matrices turns out 2 be more useful then just change syntax below
+
+
+% EXPECTED INPUT FORMAT:
+% inputs: cell array of input vectors
+% targets: cell array of output vectors
+% ---- inputs & targets should be same length, they are our training pairs ----
+% weights: cell array of weight matrices. make sure weights{1} has the same
+%           number of columns as each input has entries, and
+%           weights{length(weights)} has same number of rows as each target
+%           has entries
+% eta: a scalar between 0.01 and 0.1
 
 numLayers = length(weights);
 
 for i=1:length(inputs)
     O = inputs{i};
     OUT = cell(1, numLayers);
-    % forward pass
+    % forward pass -------------------------------------------------------
     for j=1:numLayers
         NET = weights{j} * O;  % "NET = Xw", where X is the ith matrix
                                % contained in InputWeights and w is the
@@ -17,17 +29,17 @@ for i=1:length(inputs)
         O = O.';
     end
     
-    % backward pass
+    % backward pass ------------------------------------------------------
     
     % for last layer
     output = OUT{numLayers};
-    error = output - targets{i};
+    error = abs(output - targets{i});  % dont know if abs should be there
     delta = output .* (1 - output) .* error;
     weightUpdate = eta * delta .* OUT{numLayers-1};
     weights{numLayers} = weights{numLayers} + weightUpdate;
     
     % for other hidden layers
-    for j=numLayers-1:-1:1
+    for j=numLayers-1:-1:1  % ... idk if the indexing is right here
         delta = (delta * weights{j}.') .* OUT{j} .* (1-OUT{j});
         weightUpdate = eta * delta .* OUT{j-1};
         weights{j} = weights{j} + weightUpdate;
