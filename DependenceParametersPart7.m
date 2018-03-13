99504a2b66838f5ba9b524eb73dc9f38d5da74d0
kathleneee <36493045+kathleneee@users.noreply.github.com>
2018-03-13 03:19:58 -0700

    Testing first commit

---
+++ DependenceParametersPart7.m
@@ -0,0 +1,6 @@
+% Part 7
+%  Parameters of learning of network: number of layers, neurons per layer, & size of training set 
+%  change values of these parameters and report error(s) obtained
+%  Obtain an error for the training set and another for the test test
+%   --which should be similar to each other provided the test and training set are similar enough.
+
